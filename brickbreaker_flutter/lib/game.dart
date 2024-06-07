8478f415720163995d7502ef1391e99553853b0a
kyungsang7 <kyungsangk2@gmail.com>
2024-06-07 12:54:35 +0900

    s

--- brickbreaker_flutter/lib/game.dart
+++ brickbreaker_flutter/lib/game.dart
@@ -8,6 +8,7 @@ import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 
+
 class BrickBreakerGame  extends FlameGame with HasCollisionDetection{
   static double grid = 0.1;
   static late Vector2 gridSize;
